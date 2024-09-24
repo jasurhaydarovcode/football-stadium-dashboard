3f18f0c040a7d20c5b2e5d45b9194d9b70195425
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-09-24 20:29:31 +0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import NotFound from './pages/NotFound'
-import Login from './pages/login/login'
 
 function App() {
   return (
     <React.Fragment>
       <Routes>
-        <Route path="/login" element={<Login />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </React.Fragment>
